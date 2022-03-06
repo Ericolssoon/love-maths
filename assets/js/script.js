ba28aae1540e868e2d1af36d3fd2827cd56b165e
Ericolssoon <eric_olsson@hotmail.se>
2022-03-06 10:02:48 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
-function runGame() {
 
-}
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+                    
+            }
+        })
+
+    }
+})
+
+
+   
 function checkAnswer() {
 
 }
 
 function calculateCorrectAnswer() {
 
-}
+ }
 
 function incrementScore() {
 
@@ -27,5 +45,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
